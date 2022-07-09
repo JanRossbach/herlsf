2e8465a9ffe139194f8eea3b9835c9d88d11fdb2
Jan Christopher Roßbach <jan.rossbach@hhu.de>
2022-07-09 16:21:50 +0200

    fix: Comment out dev ns

--- src/herlsf/dev.clj
+++ src/herlsf/dev.clj
@@ -13,59 +13,61 @@
 
 (comment
 
-(doc fx/sub-ctx)
+  (doc fx/sub-ctx)
 
-(doc d/db)
+  (doc d/db)
 
-(doc d/listen )
+  (doc d/listen)
 
-(doc fx/create-app)
+  (doc fx/create-app)
 
-(gui/run-app)
+  (def renderer (:renderer (gui/run-app)))
 
-((:renderer (gui/run-app)))
+  (renderer)
 
-(def conn (d/connect {:store {:backend :file
+  ((:renderer (gui/run-app)))
+
+  (def conn (d/connect {:store {:backend :file
                                 :path "resources/db/hike"}}))
 
-(def xml-src (slurp "/home/jan/School/GeheimeDaten.xml"))
+  (def xml-src (slurp "/home/jan/School/GeheimeDaten.xml"))
 
-(def data (xmlmap->map (parse (java.io.StringReader. (clojure.string/replace xml-src #"\n[ ]*|\r" "")))))
+  (def data (xmlmap->map (parse (java.io.StringReader. (clojure.string/replace xml-src #"\n[ ]*|\r" "")))))
 
-(def entities (xml->entities xml-src))
+  (def entities (xml->entities xml-src))
 
-(spec/explain ::db/entities entities)
+  (spec/explain ::db/entities entities)
 
-(def veranstaltung-ids
-'[:find ?id
-  :where
-  [?n :veranstaltung/id ?id]])
+  (def veranstaltung-ids
+    '[:find ?id
+      :where
+      [?n :veranstaltung/id ?id]])
 
-(d/q
- '[:find ?name
-   :in $ ?search
-   :where
-   [_ :veranstaltung/name ?name]
-   [(re-matches ?search ?name)]]
- conn
- (re-pattern (str ".*" "Graph" ".*")))
-
-(spit "initial_transaction.edn" entities)
-
-(d/create-database {:store {:backend :file
-                          :path "resources/db/hike"}})
-
-(d/transact conn db/schema)
-
-(d/transact conn entities)
-
-(d/q veranstaltung-ids @conn)
-
-(d/q '[:find ?id
+  (d/q
+   '[:find ?name
+     :in $ ?search
      :where
-     [?v :lehrperson/name "Klau"]
-     [?v :lehrperson/pers-id ?id]]
-   @conn)
+     [_ :veranstaltung/name ?name]
+     [(re-matches ?search ?name)]]
+   conn
+   (re-pattern (str ".*" "Graph" ".*")))
+
+  (spit "initial_transaction.edn" entities)
+
+  (d/create-database {:store {:backend :file
+                              :path "resources/db/hike"}})
+
+  (d/transact conn db/schema)
+
+  (d/transact conn entities)
+
+  (d/q veranstaltung-ids @conn)
+
+  (d/q '[:find ?id
+         :where
+         [?v :lehrperson/name "Klau"]
+         [?v :lehrperson/pers-id ?id]]
+       @conn)
 
 ;; REPL
 
@@ -77,20 +79,17 @@
   ;; ... and remove it when you are done
   (remove-watch #'styles/style :refresh-app)
 
-
-(count (d/q '[:find ?n ?m ?time ?tag ?s
-            :where
-            [?zeit :vzeit/start-zeit ?time]
-            [?zeit2 :vzeit/start-zeit ?time]
-            [?zeit :vzeit/wochentag ?tag]
-            [?zeit2 :vzeit/wochentag ?tag]
-            [?v :veranstaltung/vzeiten ?zeit]
-            [?w :veranstaltung/vzeiten ?zeit2]
-            [(not= ?v ?w)]
-            [?v :veranstaltung/studiengang ?s]
-            [?w :veranstaltung/studiengang ?s]
-            [?v :veranstaltung/name ?n]
-            [?w :veranstaltung/name ?m]]
-          @conn))
-
-)
+  (count (d/q '[:find ?n ?m ?time ?tag ?s
+                :where
+                [?zeit :vzeit/start-zeit ?time]
+                [?zeit2 :vzeit/start-zeit ?time]
+                [?zeit :vzeit/wochentag ?tag]
+                [?zeit2 :vzeit/wochentag ?tag]
+                [?v :veranstaltung/vzeiten ?zeit]
+                [?w :veranstaltung/vzeiten ?zeit2]
+                [(not= ?v ?w)]
+                [?v :veranstaltung/studiengang ?s]
+                [?w :veranstaltung/studiengang ?s]
+                [?v :veranstaltung/name ?n]
+                [?w :veranstaltung/name ?m]]
+              @conn)))
